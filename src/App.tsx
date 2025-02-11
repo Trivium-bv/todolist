d770ab1c7413763bda31231fcf1d7c47f94f6ead
Trivium-bv <trivium.bv@gmail.com>
2025-02-11 17:32:21 +0300

    add useState for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import { TaskType, Todolist } from './Todolist';
 
 function App() {
 
-  let tasks: Array<TaskType> = [
+  let initialTasks: Array<TaskType> = [
     { id: 1, title: "css", isDone: true },
     { id: 2, title: "js", isDone: true },
     { id: 3, title: "react", isDone: false },
     { id: 4, title: "redux", isDone: false },
   ]
 
+  let [tasks, setTasks] =useState(initialTasks)
+
   function removeTask(id: number) {
-    tasks = tasks.filter(t => t.id !== id )
-    debugger
+    let filteredTasks = tasks.filter(t => t.id !== id )
+    setTasks(filteredTasks)
   }
 
   return (
