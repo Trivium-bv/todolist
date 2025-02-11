728fa7d6ece6af3d30ffc18c241a796fc235bd5f
Trivium-bv <trivium.bv@gmail.com>
2025-02-11 11:50:47 +0300

    tasks.map

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { TaskType, Todolist } from './Todolist';
 
 function App() {
+
+  let tasks1: Array<TaskType> = [
+    { id: 1, title: "css", isDone: true },
+    { id: 2, title: "js", isDone: true },
+    { id: 3, title: "react", isDone: false },
+  ]
+
+  let tasks2: Array<TaskType> = [
+    { id: 1, title: "Terminator", isDone: true },
+    { id: 2, title: "Turtless", isDone: false },
+    { id: 3, title: "XXX", isDone: true },
+  ]
+
+  let tasks3: Array<TaskType> = [
+    { id: 1, title: "Dota2", isDone: true },
+    { id: 2, title: "CS GO", isDone: false },
+    { id: 3, title: "Half life", isDone: true },
+  ]
+
   return (
     <div className="App">
-      <Todolist />
-      <Todolist />
-      <Todolist />
+      <Todolist title="What to learn" tasks={tasks1} />
+      <Todolist title="Movies" tasks={tasks2} />
+      <Todolist title="Games" tasks={tasks3} />
     </div>
   );
 }
-
-function Todolist() {
-  return (
-    <div>
-      <h3>What to learn</h3>
-      <div>
-        <input />
-        <button></button>
-      </div>
-
-      <ul>
-        <li> <input type="checkbox" checked={true} /> <span>CSS&HTML</span> </li>
-        <li> <input type="checkbox" checked={true} /> <span>JS</span> </li>
-        <li> <input type="checkbox" checked={false} /> <span>React</span> </li>
-      </ul>
-
-    </div>
-  );
-}
-
 export default App;
---
+++ src/Todolist.tsx
@@ -0,0 +1,37 @@
+export type TaskType = {
+  id: number,
+  title: string,
+  isDone: boolean
+}
+
+type PropsType = {
+  title: string
+  tasks: Array<TaskType>
+}
+
+export function Todolist(props: PropsType) {
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <div>
+        <input />
+        <button>+</button>
+      </div>
+      <ul>
+        {
+          props.tasks.map(t => <li><input type="checkbox" checked={t.isDone} />
+            <span>{t.title}</span>
+            <button onClick={() =>{ alert (t.id)}}>x</button>
+          </li>
+          )
+        }
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+
+    </div>
+  );
+}
